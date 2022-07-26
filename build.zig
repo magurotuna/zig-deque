8f1357ca3957ac8925decb5680725f8de36315f2
Yusuke Tanaka <yusuktan@maguro.dev>
2022-07-26 21:38:11 +0900

    rename main.zig to deque.zig

--- build.zig
+++ build.zig
@@ -5,11 +5,11 @@ pub fn build(b: *std.build.Builder) void {
     // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall.
     const mode = b.standardReleaseOptions();
 
-    const lib = b.addStaticLibrary("zig-deque", "src/main.zig");
+    const lib = b.addStaticLibrary("zig-deque", "src/deque.zig");
     lib.setBuildMode(mode);
     lib.install();
 
-    const main_tests = b.addTest("src/main.zig");
+    const main_tests = b.addTest("src/deque.zig");
     main_tests.setBuildMode(mode);
 
     const test_step = b.step("test", "Run library tests");
